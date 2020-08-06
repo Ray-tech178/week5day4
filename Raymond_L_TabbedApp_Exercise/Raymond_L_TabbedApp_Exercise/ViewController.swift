fb5a90c1d9620663c21dcbce58013752074e2ac3
Ray-tech178 <67976062+Ray-tech178@users.noreply.github.com>
2020-08-06 14:13:10 -0500

    Add files via upload

---
+++ Raymond_L_TabbedApp_Exercise/Raymond_L_TabbedApp_Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Raymond_L_TabbedApp_Exercise
+//
+//  Created by Raymond Lo on 8/6/20.
+//  Copyright © 2020 Raymond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
